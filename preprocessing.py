18e84c233bd9ee23953533c3c2b96ab854d4317b
michaelchristenson <mchristenson@apple.com>
2023-11-02 21:01:45 -0700

    fixed some file name parsing issues and added initial calculation of histograms

--- preprocessing.py
+++ preprocessing.py
@@ -1,10 +1,11 @@
 import os
 import pandas as pd
+import cv2
 
 
-def image_dataframe(image_path):
-    val_path = f'{image_path}/val'
-    train_path = f'{image_path}/train'
+def image_dataframe(images_path):
+    val_path = f'{images_path}/val'
+    train_path = f'{images_path}/train'
     classes = [f for f in os.listdir(val_path) if f != '.DS_Store']
     image_data = []
     for img_class in classes:
@@ -25,10 +26,13 @@ def image_dataframe(image_path):
     # reset so class 1 is class 0, this will jive better with soft max outputs
     split_df['class_number'] = pd.to_numeric(split_df['class_number']) - 1
     split_df['latitude'] = pd.to_numeric(split_df['latitude'])
+    split_df['longitude'] = list(map(lambda x: x.replace('.png', ''), split_df['longitude']))
     split_df['longitude'] = pd.to_numeric(split_df['longitude'])
     split_df['file_name'] = combined_df['file_name']
     split_df['class'] = combined_df['class']
     split_df['split'] = combined_df['split']
+    split_df['full_path'] = split_df.apply(lambda x: f'{images_path}/{x["split"]}/{x["class"]}/{x["file_name"]}', axis=1)
     split_df.to_csv('images.csv')
     return split_df
+
 
